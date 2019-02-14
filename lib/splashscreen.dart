dafc62ac8bb63fc99cd0af5c8611aa22e3aa7dcd
raminnietzsche <ramin.najarbashi@gmail.com>
2019-02-14 12:29:46 +0330

    Remove timer and add loadData function triger ;)

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,14 +1,11 @@
 library splashscreen;
 import 'dart:core';
-import 'dart:async';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  final int seconds;
   final Text title;
   final Color backgroundColor;
   final TextStyle styleTextUnderTheLoader;
-  final dynamic navigateAfterSeconds;
   final double photoSize;
   final dynamic onClick;
   final Color loaderColor;
@@ -16,13 +13,12 @@ class SplashScreen extends StatefulWidget {
   final Text loadingText;
   final ImageProvider imageBackground;
   final Gradient gradientBackground;
+  final Function loadingData;
   SplashScreen(
       {
         this.loaderColor,
-        @required this.seconds,
         this.photoSize,
         this.onClick,
-        this.navigateAfterSeconds,
         this.title = const Text(''),
         this.backgroundColor = Colors.white,
         this.styleTextUnderTheLoader = const TextStyle(
@@ -33,7 +29,8 @@ class SplashScreen extends StatefulWidget {
         this.image,
         this.loadingText  = const Text(""),
         this.imageBackground,
-      	this.gradientBackground
+        this.gradientBackground,
+        this.loadingData
       }
       );
 
@@ -45,23 +42,21 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
+    var route;
     super.initState();
-    Timer(
-        Duration(seconds: widget.seconds),
-            () {
-          if (widget.navigateAfterSeconds is String) {
-            // It's fairly safe to assume this is using the in-built material
-            // named route component
-            Navigator.of(context).pushReplacementNamed(widget.navigateAfterSeconds);
-          } else if (widget.navigateAfterSeconds is Widget) {
-            Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (BuildContext context) => widget.navigateAfterSeconds));
-          } else {
-            throw new ArgumentError(
-                'widget.navigateAfterSeconds must either be a String or Widget'
-            );
-          }
-        }
-    );
+    widget.loadingData().then((r){
+      route = r;
+    }).whenComplete((){
+      if (route is String) {
+        Navigator.of(context).pushReplacementNamed(route);
+      } else if (route is Widget) {
+        Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (BuildContext context) => route));
+      } else {
+        throw new ArgumentError(
+            'widget.navigateAfterSeconds must either be a String or Widget'
+        );
+      }
+    });
   }
   @override
   Widget build(BuildContext context) {
@@ -76,9 +71,9 @@ class _SplashScreenState extends State<SplashScreen> {
                 image: widget.imageBackground == null
                     ? null
                     : new DecorationImage(
-                        fit: BoxFit.cover,
-                        image: widget.imageBackground,
-                      ),
+                  fit: BoxFit.cover,
+                  image: widget.imageBackground,
+                ),
                 gradient: widget.gradientBackground,
                 color: widget.backgroundColor,
               ),
