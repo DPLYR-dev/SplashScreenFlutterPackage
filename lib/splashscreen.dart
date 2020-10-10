c52e0b07c76462bb429e3ebc99283fba138d4de3
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-10-10 09:06:23 +0200

    format files

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,4 +1,5 @@
 library splashscreen;
+
 import 'dart:core';
 import 'dart:async';
 import 'package:flutter/material.dart';
@@ -17,28 +18,22 @@ class SplashScreen extends StatefulWidget {
   final ImageProvider imageBackground;
   final Gradient gradientBackground;
   final bool useLoader;
-  SplashScreen(
-      {
-        this.loaderColor,
-        @required this.seconds,
-        this.photoSize,
-        this.onClick,
-        this.navigateAfterSeconds,
-        this.title = const Text(''),
-        this.backgroundColor = Colors.white,
-        this.styleTextUnderTheLoader = const TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black
-        ),
-        this.image,
-        this.loadingText  = const Text(""),
-        this.imageBackground,
-      	this.gradientBackground,
-        this.useLoader = true,
-      }
-      );
-
+  SplashScreen({
+    this.loaderColor,
+    @required this.seconds,
+    this.photoSize,
+    this.onClick,
+    this.navigateAfterSeconds,
+    this.title = const Text(''),
+    this.backgroundColor = Colors.white,
+    this.styleTextUnderTheLoader = const TextStyle(
+        fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black),
+    this.image,
+    this.loadingText = const Text(""),
+    this.imageBackground,
+    this.gradientBackground,
+    this.useLoader = true,
+  });
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -48,29 +43,27 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(
-        Duration(seconds: widget.seconds),
-            () {
-          if (widget.navigateAfterSeconds is String) {
-            // It's fairly safe to assume this is using the in-built material
-            // named route component
-            Navigator.of(context).pushReplacementNamed(widget.navigateAfterSeconds);
-          } else if (widget.navigateAfterSeconds is Widget) {
-            Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (BuildContext context) => widget.navigateAfterSeconds));
-          } else {
-            throw new ArgumentError(
-                'widget.navigateAfterSeconds must either be a String or Widget'
-            );
-          }
-        }
-    );
+    Timer(Duration(seconds: widget.seconds), () {
+      if (widget.navigateAfterSeconds is String) {
+        // It's fairly safe to assume this is using the in-built material
+        // named route component
+        Navigator.of(context).pushReplacementNamed(widget.navigateAfterSeconds);
+      } else if (widget.navigateAfterSeconds is Widget) {
+        Navigator.of(context).pushReplacement(new MaterialPageRoute(
+            builder: (BuildContext context) => widget.navigateAfterSeconds));
+      } else {
+        throw new ArgumentError(
+            'widget.navigateAfterSeconds must either be a String or Widget');
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: new InkWell(
         onTap: widget.onClick,
-        child:new Stack(
+        child: new Stack(
           fit: StackFit.expand,
           children: <Widget>[
             new Container(
@@ -92,34 +85,34 @@ class _SplashScreenState extends State<SplashScreen> {
                   flex: 2,
                   child: new Container(
                       child: new Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          new CircleAvatar(
-                            backgroundColor: Colors.transparent,
-                            child: Hero(
-                              tag: "splashscreenImage",
-                                child: new Container(
-                                  child: widget.image
-                              ),
-                            ),
-                            radius: widget.photoSize,
-                          ),
-                          new Padding(
-                            padding: const EdgeInsets.only(top: 10.0),
-                          ),
-                          widget.title
-                        ],
-                      )),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        child: Hero(
+                          tag: "splashscreenImage",
+                          child: new Container(child: widget.image),
+                        ),
+                        radius: widget.photoSize,
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                      ),
+                      widget.title
+                    ],
+                  )),
                 ),
                 Expanded(
                   flex: 1,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-
-                      !widget.useLoader ? Container() :CircularProgressIndicator(
-                        valueColor: new AlwaysStoppedAnimation<Color>(widget.loaderColor),
-                      ),
+                      !widget.useLoader
+                          ? Container()
+                          : CircularProgressIndicator(
+                              valueColor: new AlwaysStoppedAnimation<Color>(
+                                  widget.loaderColor),
+                            ),
                       Padding(
                         padding: const EdgeInsets.only(top: 20.0),
                       ),
